73c95720986ca7cab25e694e866f2dc6fd68e10a
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-18 14:49:39 +0800

    refactor test

--- src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
@@ -1,27 +1,13 @@
 package com.thoughtworks.kotlin_basic.practice
 
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
-import kotlin.test.assertFails
 import kotlin.test.assertFailsWith
 
 class SolutionTest {
     val solution = Solution()
-
-    @Test
-    fun `should get the corresponding letter A-Z when input any sequence of 1-26 and the size is 1`() {
-        assertContentEquals(listOf("A"), solution.transform(1, 1))
-        assertContentEquals(listOf("B"), solution.transform(2, 1))
-        assertContentEquals(listOf("G"), solution.transform(7, 1))
-        assertContentEquals(listOf("X"), solution.transform(24, 1))
-        assertContentEquals(listOf("Z"), solution.transform(26, 1))
-    }
-
-    @Test
-    fun `should get x letter when input any sequence of 1-26 and size is x`() {
-        assertContentEquals(listOf("B", "C", "D"), solution.transform(2, 3))
-        assertContentEquals(listOf("X", "Y"), solution.transform(24, 2))
-    }
 
     @Test
     fun `should throw exception when invalid argument`() {
@@ -35,13 +21,31 @@ class SolutionTest {
         }
     }
 
-    @Test
-    fun `should support A to ZZZ when input any sequence`() {
-        assertContentEquals(listOf("AA"), solution.transform(27, 1))
-        assertContentEquals(listOf("AB"), solution.transform(28, 1))
-        assertContentEquals(listOf("Z", "AA", "AB"), solution.transform(26, 3))
-        assertContentEquals(listOf("ZZZ"), solution.transform(18278, 1))
-        assertContentEquals(listOf("ZZY", "ZZZ"), solution.transform(18277, 2))
-        assertContentEquals(listOf("ZBZ"), solution.transform(26*26*26+26*2+26, 1))
+    companion object {
+        @JvmStatic
+        fun testCaseProvider(): List<Pair<Pair<Int, Int>, List<String>>> {
+            return listOf(
+                Pair(1, 1) to listOf("A"),
+                Pair(2, 1) to listOf("B"),
+                Pair(7, 1) to listOf("G"),
+                Pair(24, 1) to listOf("X"),
+                Pair(26, 1) to listOf("Z"),
+                Pair(2, 3) to listOf("B", "C", "D"),
+                Pair(24, 4) to listOf("X", "Y", "Z", "AA"),
+                Pair(27, 1) to listOf("AA"),
+                Pair(28, 1) to listOf("AB"),
+                Pair(18278, 1) to listOf("ZZZ"),
+                Pair(18277, 2) to listOf("ZZY", "ZZZ"),
+                Pair(26 * 26 * 26 + 26 * 2 + 26, 1) to listOf("ZBZ"),
+                Pair(30, 11) to listOf("AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN")
+            )
+        }
     }
+
+    @ParameterizedTest
+    @MethodSource("testCaseProvider")
+    fun abstractTest(case: Pair<Pair<Int, Int>, List<String>>) {
+        assertContentEquals(case.second, solution.transform(case.first.first, case.first.second))
+    }
+
 }
