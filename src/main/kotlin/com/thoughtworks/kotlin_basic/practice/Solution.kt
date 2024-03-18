7776a56ad3065a309f754070c419131632c9c272
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-18 13:17:17 +0800

    should get the corresponding letter A-Z

--- src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
@@ -2,6 +2,6 @@ package com.thoughtworks.kotlin_basic.practice
 
 class Solution {
     fun transform(index: Int, size: Int): List<String> {
-        return listOf("A")
+        return listOf((index+64).toChar().toString())
     }
 }
--- src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
@@ -9,4 +9,12 @@ class SolutionTest {
     fun `should get A when input (1,1)`() {
         assertContentEquals(listOf("A"), solution.transform(1, 1))
     }
+
+    @Test
+    fun `should get the corresponding letter A-Z when input any sequence of 1-26 and the size is 1`() {
+        assertContentEquals(listOf("B"), solution.transform(2, 1))
+        assertContentEquals(listOf("G"), solution.transform(7, 1))
+        assertContentEquals(listOf("X"), solution.transform(24, 1))
+        assertContentEquals(listOf("Z"), solution.transform(26, 1))
+    }
 }
