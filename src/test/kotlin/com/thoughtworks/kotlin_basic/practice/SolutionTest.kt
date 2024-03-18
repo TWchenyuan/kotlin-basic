4dff3874ba8726b06d231c0b141beeb92a01864d
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-18 13:32:53 +0800

    refactor test

--- src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
@@ -6,12 +6,8 @@ import kotlin.test.assertContentEquals
 class SolutionTest {
     val solution = Solution()
     @Test
-    fun `should get A when input (1,1)`() {
-        assertContentEquals(listOf("A"), solution.transform(1, 1))
-    }
-
-    @Test
     fun `should get the corresponding letter A-Z when input any sequence of 1-26 and the size is 1`() {
+        assertContentEquals(listOf("A"), solution.transform(1, 1))
         assertContentEquals(listOf("B"), solution.transform(2, 1))
         assertContentEquals(listOf("G"), solution.transform(7, 1))
         assertContentEquals(listOf("X"), solution.transform(24, 1))
