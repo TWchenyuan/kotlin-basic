67cf35dd21372d96a50584cdf3cfee920bf8e724
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-18 13:42:09 +0800

    throw exception when illegal input

--- src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.kotlin_basic.practice
 
 class Solution {
     fun transform(index: Int, size: Int): List<String> {
+        if (index >= 18279 || size >= 18279 || (index + size - 1) >= 18279) throw IllegalArgumentException()
         return (index..<index + size).map {
             it.toASCIICode()
         }
--- src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/practice/SolutionTest.kt
@@ -2,6 +2,8 @@ package com.thoughtworks.kotlin_basic.practice
 
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
+import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
 
 class SolutionTest {
     val solution = Solution()
@@ -18,5 +20,14 @@ class SolutionTest {
     fun `should get x letter when input any sequence of 1-26 and size is x`() {
         assertContentEquals(listOf("B", "C", "D"), solution.transform(2, 3))
         assertContentEquals(listOf("X", "Y"), solution.transform(24, 2))
+    }
+
+    @Test
+    fun `should throw exception when index plus size greater than 18278`() {
+        assertFailsWith<IllegalArgumentException> {
+            solution.transform(18279,1)
+            solution.transform(18278,2)
+            solution.transform(1, 18278)
+        }
     }
 }
