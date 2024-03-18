e1f1c83398e8e7531e38f6a13bb7b26bd50510c4
Chenyuan Hu <chenyuan.hu@thoughtworks.com>
2024-03-18 14:55:08 +0800

    refactor solution

--- src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/practice/Solution.kt
@@ -3,24 +3,17 @@ package com.thoughtworks.kotlin_basic.practice
 class Solution {
     fun transform(index: Int, size: Int): List<String> {
         if (isInValidInput(index, size)) throw IllegalArgumentException()
-        val a = (index..<index + size).map {
+        return (index..<index + size).map {
             var current = it - 1
             var result = ""
-            while (true) {
-                if (current < 0) break
-                if (current < 26) {
-                    result = current.toASCIICode() + result
-                    break
-                } else {
-                    val nextCurrent = current / 26
-                    val reminder = current % 26
-                    result = reminder.toASCIICode() + result
-                    current = nextCurrent - 1
-                }
+            while (current >= 0) {
+                val nextCurrent = current / 26
+                val reminder = current % 26
+                result = reminder.toASCIICode() + result
+                current = nextCurrent - 1
             }
             result
         }
-        return a
     }
 
     private fun isInValidInput(index: Int, size: Int): Boolean =
